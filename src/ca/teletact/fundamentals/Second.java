94661d68071d6fc911122f59457c0c9632d73248
Siv Maharaj <sivatma.maharaj@rogers.com>
2023-05-10 22:23:30 -0400

    added Second.java to the project

---
+++ src/ca/teletact/fundamentals/Second.java
@@ -0,0 +1,9 @@
+package ca.teletact.fundamentals;
+
+public class Second{
+
+    public static void main(String args[]){
+        System.out.println("This is the second java program in this project.");
+        
+    }
+}
